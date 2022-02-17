7981d4531cffd810d338da5e2a5dc9908186ae9a
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-17 20:27:31 +0200

    Correct Rubocop offenses

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "posts controller", type: :request do
-  describe "GET posts routes" do
-    describe "GET /users/:user_id/posts" do
-      before(:example) { get "/users/1/posts" }
+RSpec.describe 'posts controller', type: :request do
+  describe 'GET posts routes' do
+    describe 'GET /users/:user_id/posts' do
+      before(:example) { get '/users/1/posts' }
 
-      it "should return 200 http_status_code" do
+      it 'should return 200 http_status_code' do
         expect(response).to have_http_status(:ok)
       end
 
-      it "should render the correct template" do
+      it 'should render the correct template' do
         expect(response).to render_template(:index)
       end
     end
 
-    describe "GET /users/:user_id/posts/:id" do
-      before(:example) { get "/users/1/posts/1" }
+    describe 'GET /users/:user_id/posts/:id' do
+      before(:example) { get '/users/1/posts/1' }
 
-      it "should return 200 http_status_code" do
+      it 'should return 200 http_status_code' do
         expect(response).to have_http_status(:ok)
       end
 
-      it "should render the correct template" do
+      it 'should render the correct template' do
         expect(response).to render_template(:show)
       end
     end
