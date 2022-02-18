ea4017d90f75177a52435ce1f665ddea396e1298
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-19 00:53:08 +0200

    Migrate posts db to create schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_18_203801) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_18_221835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "likes_counter"
+    t.integer "comments_counter"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
