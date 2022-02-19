1d5511edc5a625f875ce1b63aa438368278a30fe
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-19 10:00:12 +0200

    Configure likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new]
+    resources :posts, only: [:index, :show, :new] do
+      resources :likes, only: [:new]
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
