d164bc6dd0799983b6cbd423afc25fb051ccce59
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-19 10:51:54 +0200

    Migrate db to create comments schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_19_072609) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_19_082611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.bigint "author_id"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "author_id"
@@ -41,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_072609) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
 end
