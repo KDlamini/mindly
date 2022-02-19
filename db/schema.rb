adcb88e070c032b3696dcca55fb3f267f0434bd7
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-19 09:57:04 +0200

    Migrate db to create schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_18_221835) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_19_072609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.integer "likes_counter"
     t.integer "comments_counter"
-    t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_18_221835) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
 end
