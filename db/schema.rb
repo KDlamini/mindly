a2627daf43d4e2efe2603bebe7030cf892aa7423
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-19 11:02:12 +0200

    Update schema with indexing

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_082611) do
     t.text "text"
     t.integer "likes_counter"
     t.integer "comments_counter"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_082611) do
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
