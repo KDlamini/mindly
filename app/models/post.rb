355bb7a931757d81f22e43465d5f2a9ff97bdf7b
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-22 16:31:06 +0200

    Fix Rubocop offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ApplicationRecord
-    after_save :update_posts_counter
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: 'User'
+  after_save :update_posts_counter
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
-    validates :title, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
 
-    def update_posts_counter
-        author.increment!(:posts_counter)
-    end
-    
-    def most_recent_comments
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
     comments.order('created_at DESC').limit(5)
-    end
+  end
 end
