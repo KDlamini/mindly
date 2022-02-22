162bdba79f3f2075eb5b21e9c9e10d112e4138e6
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-23 01:06:20 +0200

    Add user partial and update controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   # GET /users
-  def index; end
+  def index
+    @users = User.all
+    @current_user = current_user
+  end
 
   # GET /users/1
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
