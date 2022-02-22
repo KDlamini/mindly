4e0444b5af135e420be0ee1ffdf38c96c96aabfe
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-22 16:29:00 +0200

    Add method that updates the comments counter for a post Add method that updates the likes counter for a post.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_counter
   belongs_to :post
   belongs_to :author, class_name: 'User'
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
