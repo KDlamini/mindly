64ccae07180125e25b4317ac85cfeae3b5466356
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-23 10:33:33 +0200

    Add posts view and update controller methods.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   # GET /users/posts
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+    @comments = Comment.new
+  end
 
   # GET /users/1/posts/1
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id]).where(author_id: params[:user_id])
+  end
 end
