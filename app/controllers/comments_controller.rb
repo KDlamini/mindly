173b56c2ce07102f00180c03bfb79586255e0be5
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-24 20:32:40 +0200

    Update comments controller with creation methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,27 @@
 class CommentsController < ApplicationController
   # GET /comments/new
-  def new; end
+  def new
+    @comment = Comment.new
+    set_comment
+  end
 
   # GET /comments/1/edit
   def edit; end
 
   # POST /comments
-  def create; end
+  def create
+    @current_user = User.find(params[:user_id])
+    @comment = @current_user.comments.new(comment_params)
+    set_comment
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to user_post_path(@comment.author_id, @comment.post_id), notice: "Comment was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
   # PATCH/PUT /comments/1
   def update; end
@@ -15,10 +30,12 @@ class CommentsController < ApplicationController
   def destroy; end
 
   private
+    def set_comment
+      @comment.post_id = params[:post_id]
+      @comment.author_id = params[:user_id]
+    end
 
-  # Find the comment by id
-  def set_comment; end
-
-  # Allow a list of trusted parameters through.
-  def comment_params; end
+    def comment_params
+      params.require(:comment).permit(:text, :post_id)
+    end
 end
