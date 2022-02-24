ebf093eedd9cbf2d9a6ca0ea0e2aebaae26f4f34
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-24 22:02:54 +0200

    Update likes controller with create method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,18 @@
 class LikesController < ApplicationController
   # POST /likes
-  def create; end
+  def create
+    set_like
+    @like = @current_user.likes.new(author_id: params[:user_id], post_id: params[:post_id])
+
+    respond_to do |format|
+      format.html { redirect_to user_post_path(@like.author_id, @like.post_id), notice: "You liked this post." } if @like.save
+    end
+  end
+
+  private
+
+  def set_like
+    @like = Like.new
+    @current_user = User.find(params[:user_id])
+  end
 end
