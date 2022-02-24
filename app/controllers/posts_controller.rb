e1b624bf76b7544fa06b6f2a22c7311029920462
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-02-24 18:15:04 +0200

    Update posts controller with new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,36 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id]).where(author_id: params[:user_id])
   end
+
+  # GET /users/1/posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @current_user = User.find(params[:user_id])
+    @post = @current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_posts_path, notice: "Post was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :likes_counter, :comments_counter)
+  end
+
+  def reference_params
+    @post.author_id = params[:user_id]
+  end
 end
