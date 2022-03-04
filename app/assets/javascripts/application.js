1be3dc8dff1f8a34c7ef6e68b15c6f0adaebfc05
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-04 19:26:01 +0200

    Update Foundation application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,2 @@
 //= require foundation
-$(function(){ $(document).foundation(
-    {
-        orbit: {
-            animation: 'slide',
-            timer_speed: 10000,
-            pause_on_hover: true,
-            animation_speed: 500,
-            navigation_arrows: true,
-            bullets: false
-        }
-    }
-); });
+$(function(){ $(document).foundation(); });
